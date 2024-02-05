1b908a94b5d7ac1af4fe9ae99876f09c0c0f4dd2
unknown <soa548277@gmail.com>
2024-02-05 08:17:05 +0200

    second commit

--- typescript/grafbase.config.ts
+++ typescript/grafbase.config.ts
@@ -1,12 +1,5 @@
 import { graph, config } from '@grafbase/sdk';
 
-// Define your own type for the configuration object
-interface MyGraphConfig {
-  graph: any; // You may need to replace 'any' with the appropriate type
-  models?: any[]; // You may need to replace 'any' with the appropriate type
-  // Add any other properties you need for the configuration
-}
-
 // Welcome to Grafbase!
 //
 // Configure authentication, data sources, resolvers, and caching for your GraphQL API.
@@ -18,27 +11,23 @@ const User = g.model("User", {
   name: g.string().length({min:2 , max:20}),
   email: g.string().unique(),
   avatarUrl: g.url(),
-  description: g.string().optional(),
+  description:g.string().optional(),
   githubUrl: g.url().optional(),
   linkedIn: g.url().optional(),
-  projects:g.relation(()=>Projects).list().optional(),
+  projects: g.relation(()=>Projects).list().optional(),
 });
 
 const Projects = g.model("Projects", {
   title: g.string().length({min:3}),
-  image: g.url(),
-  description: g.string().optional(),
-  liveSiteUrl: g.url(),
+  image:g.url(),
+  description:g.string().optional(),
+  liveSiteUrl:g.url(),
   githubUrl: g.url(),
   category: g.string().search(),
-  categoryBy: g.relation(()=>User),
+  categoryBy:g.relation(()=>User),
+
+})
+
+export default config({
+  schema: g,
 });
-
-// Define your configuration object
-const graphConfig: MyGraphConfig = {
-  graph: g,
-  models: [User, Projects], // Pass the User and Projects models to the config
-  // Add any other properties as needed
-};
-
-export default config(graphConfig);
